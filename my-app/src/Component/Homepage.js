2958408635a2a7b49e59366ee71c64742323b9ac
hongquan <danghongquanvtc1111@gmail.com>
2024-06-04 08:42:39 +0700

    update

--- my-app/src/Component/Homepage.js
+++ my-app/src/Component/Homepage.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import Navbar from './Navbar';
+import React from "react";
+import "./Css/Homepage.css";
+import Navbar from "./Navbar";
 const Homepage = () => {
-    return (
-        <div id='homepage'>
-            <Navbar/>
+  return (
+    <div id="homepage">
+      <Navbar />
+      <section id="introduce">
+        <div className="intro-container">
+          <div className="intro-left">
+            <h1 className="title">
+              <mark className="Improve">
+                Improve
+                <img src="assets/svg/headline-curve.svg" alt="svg" />
+              </mark>
+              your
+              <span>Skill</span>
+              <img
+                src="assets/svg/svgexport-2.svg"
+                style={{ width: "23px", height: "26px" }}
+                alt="svg-top-skill"
+              />
+            </h1>
+          </div>
+          <div className="intro-right">
+            <img src="assets/img/home-hero.webp" alt="intro-image" />
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
-export default Homepage ;
+export default Homepage;
