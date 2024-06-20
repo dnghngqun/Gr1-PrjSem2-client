982c5ac43da441e3edb5b20271f4655b9eed9b14
hongquan <danghongquanvtc1111@gmail.com>
2024-06-20 23:01:23 +0700

    up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Course from "./Component/Course";
 import Homepage from "./Component/Homepage";
+import Login from "./Component/Login";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/course" element={<Course />}></Route>
+        <Route path="/login" element={<Login/>}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ my-app/src/Component/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Css/Login.css';
+const Login = () => {
+    return (
+        <div id='login'>
+            <div className="login-container">
+                <div className="left">
+                    <img src="assets/img/images_login.png" alt="..." />
+                </div>
+                <div className="right"></div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Login;
