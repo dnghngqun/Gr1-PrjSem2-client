cb72d2763ca6b61267b012a96eed7e7e499bab48
hongquan <danghongquanvtc1111@gmail.com>
2024-06-22 07:54:35 +0700

    update login page

--- my-app/src/Component/Login.js
+++ my-app/src/Component/Login.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import './Css/Login.css';
+import React from "react";
+import "./Css/Login.css";
 const Login = () => {
-    return (
-        <div id='login'>
-            <div className="login-container">
-                <div className="left">
-                    <img src="assets/img/images_login.png" alt="..." />
-                </div>
-                <div className="right"></div>
-
-            </div>
+  return (
+    <div id="login">
+      <div className="login-container">
+        <div className="left">
+          <img src="assets/img/images_login.png" alt="..." />
         </div>
-    );
+        <div className="right">
+          <h1 className="title">Member Login</h1>
+          <form action="POST" className="login-by-form">
+            <input
+              type="text"
+              placeholder="Username, email or phone number"
+              className="btn username"
+            />
+
+            <input
+              type="password"
+              className="btn password"
+              placeholder="Password"
+            />
+
+            <button type="submit" className=" btn btn-submit">
+              <b>Sign in</b>
+            </button>
+
+            <a href="#" className="forgot">
+              Forgot <span>Username/Password?</span>
+            </a>
+          </form>
+          <b className="text-line">or sign in with</b>
+          <div className="login-other"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
