e24f71267df98534b8ab0925b60ffce1221927d3
hongquan <danghongquanvtc1111@gmail.com>
2024-07-05 22:16:54 +0700

    update

---
+++ my-app/src/Component/Thanks.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+const Thanks = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Thank You!</h1>
+      <p>Your payment was successful. Thank you for your purchase!</p>
+      <Footer />
+    </div>
+  );
+};
+
+export default Thanks;
