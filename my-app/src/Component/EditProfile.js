b0f95e44aa99845d7d7b1517ed66a052dcb9ced1
dothaofpt <143236377+dothaofpt@users.noreply.github.com>
2024-07-23 11:31:01 +0700

    up

--- my-app/src/Component/EditProfile.js
+++ my-app/src/Component/EditProfile.js
@@ -8,6 +8,7 @@ import "toastify-js/src/toastify.css";
 import "./Css/EditProfile.css";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import ForgotPassword from './ForgotPassword';
 const EditProfile = ({ isLoggedIn, onLogout, triggerRefresh }) => {
   const [fullname, setFullname] = useState(isLoggedIn.data.fullName);
   const [phoneNumber, setPhoneNumber] = useState(isLoggedIn.data.phoneNumber);
@@ -21,7 +22,8 @@ const EditProfile = ({ isLoggedIn, onLogout, triggerRefresh }) => {
   const [imageFile, setImageFile] = useState(null);
   const [message, setMessage] = useState("Hello");
   const [imageUrl, setImageUrl] = useState(isLoggedIn.data.imageAccount);
-
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
   const notify = (mess) =>
     toast({
       text: mess,
@@ -149,117 +151,182 @@ const EditProfile = ({ isLoggedIn, onLogout, triggerRefresh }) => {
         notifyFail("Error to update information!");
       });
   };
+  const handleSubmitPassword = () => {
+    const changePass = {
+      identify: isLoggedIn.data.userName,
+      oldPassword: oldPassword,
+      newPassword: newPassword,
+    };
+    console.log(changePass);
+    axios
+      .put("http://localhost:8080/api/v1/accounts/changePassword", null, {
+        params: changePass,
+      })
+      .then((res) => notify("Change password successfully!"))
+      .catch((err) => {
+        console.log("Error to change pass: ", err);
+        notifyFail("Old password is incorrect!");
+      });
+  };
+
   return (
     <div>
-      <Navbar isLoggedIn={isLoggedIn} onLogout={onLogout} />
-      <div className="edit-profile">
-        <div className="avatar-profile">
-          <div className="avt-container">
-            <img src={imageUrl} className="img-avt" alt="" />
-            <div className="upload-btn">
-              <label htmlFor="upload-button" className="upload-icon">
-                <img src="/assets/svg/camera-solid.svg" alt="" />
-              </label>
-              <input
-                type="file"
-                accept="image/*"
-                onChange={handleImageChange}
-                style={{ display: "none" }}
-                id="upload-button"
-              />
+      <div>
+        <Navbar isLoggedIn={isLoggedIn} onLogout={onLogout} />
+        <div className="edit-profile">
+          <div className="avatar-profile">
+            <div className="avt-container">
+              <img src={imageUrl} className="img-avt" alt="" />
+              <div className="upload-btn">
+                <label htmlFor="upload-button" className="upload-icon">
+                  <img src="/assets/svg/camera-solid.svg" alt="" />
+                </label>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  style={{ display: "none" }}
+                  id="upload-button"
+                />
+              </div>
             </div>
           </div>
+          <form className="form-edit">
+            <label htmlFor="fullname" className="label-edit">
+              Fullname:
+            </label>
+            <br />
+            <input
+              type="text"
+              id="fullname"
+              value={fullname}
+              onChange={(e) => setFullname(e.target.value)}
+            />
+            <br />
+            <label htmlFor="email" className="label-edit">
+              Email:
+            </label>
+            <br />
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            <label htmlFor="phoneNumber" className="label-edit">
+              Phone Number:{" "}
+            </label>
+            <br />
+            <input
+              type="text"
+              id="phoneNumber"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+            <br />
+            <label htmlFor="" className="label-edit">
+              Birthday
+            </label>
+            <div className="date-birthday">
+              <div className="date">
+                <label htmlFor="day" className="day label-edit">
+                  Day:
+                </label>
+                <br />
+                <input
+                  type="number"
+                  min="1"
+                  max="31"
+                  step="1"
+                  id="day"
+                  value={day}
+                  onChange={handleDayChange}
+                />
+              </div>
+              <div className="date">
+                <label htmlFor="month" className="month label-edit">
+                  Month:
+                </label>
+                <br />
+                <input
+                  type="number"
+                  min="1"
+                  max="12"
+                  step="1"
+                  id="month"
+                  value={month}
+                  onChange={handleMonthChange}
+                />
+              </div>
+              <div className="date">
+                <label htmlFor="month" className="year label-edit">
+                  Year:
+                </label>
+                <br />
+                <input
+                  type="number"
+                  min="1930"
+                  max="2099"
+                  step="1"
+                  id="year"
+                  value={year}
+                  onChange={handleYearChange}
+                />
+              </div>
+            </div>
+            <a onClick={handleSubmit} className="btn-edit">
+              Update information
+            </a>
+          </form>
         </div>
-        <form className="form-edit">
-          <label htmlFor="fullname" className="label-edit">
-            Fullname:
-          </label>
-          <br />
-          <input
-            type="text"
-            id="fullname"
-            value={fullname}
-            onChange={(e) => setFullname(e.target.value)}
-          />
-          <br />
-          <label htmlFor="email" className="label-edit">
-            Email:
-          </label>
-          <br />
-          <input
-            type="text"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <br />
-          <label htmlFor="phoneNumber" className="label-edit">
-            Phone Number:{" "}
-          </label>
-          <br />
-          <input
-            type="text"
-            id="phoneNumber"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-          />
-          <br />
-          <label htmlFor="" className="label-edit">
-            Birthday
-          </label>
-          <div className="date-birthday">
-            <div className="date">
-              <label htmlFor="day" className="day label-edit">
-                Day:
-              </label>
-              <br />
-              <input
-                type="number"
-                min="1"
-                max="31"
-                step="1"
-                id="day"
-                value={day}
-                onChange={handleDayChange}
-              />
-            </div>
-            <div className="date">
-              <label htmlFor="month" className="month label-edit">
-                Month:
-              </label>
-              <br />
-              <input
-                type="number"
-                min="1"
-                max="12"
-                step="1"
-                id="month"
-                value={month}
-                onChange={handleMonthChange}
-              />
-            </div>
-            <div className="date">
-              <label htmlFor="month" className="year label-edit">
-                Year:
-              </label>
-              <br />
-              <input
-                type="number"
-                min="1930"
-                max="2099"
-                step="1"
-                id="year"
-                value={year}
-                onChange={handleYearChange}
-              />
+        <div className="edit-profile">
+          <div className="avatar-profile">
+            <div className="avt-container">
+              <img src={imageUrl} className="img-avt" alt="" />
+              <div className="upload-btn">
+                <label htmlFor="upload-button" className="upload-icon">
+                  <img src="/assets/svg/camera-solid.svg" alt="" />
+                </label>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  style={{ display: "none" }}
+                  id="upload-button"
+                />
+              </div>
             </div>
           </div>
-          <a onClick={handleSubmit} className="btn-edit">
-            Update information
-          </a>
-        </form>
+          <form className="form-edit">
+            <h1>
+              <trong>Change Password</trong>
+            </h1>
+            <form>
+              <div>
+                <label>Old Password:</label>
+                <input
+                  type="password"
+                  value={oldPassword}
+                  onChange={(e) => setOldPassword(e.target.value)}
+                />
+              </div>
+              <div>
+                <label>New Password:</label>
+                <input
+                  type="password"
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                />
+              </div>
+              <a onClick={handleSubmit} className="btn-edit">
+             Change Password
+            </a>
+            </form>
+          </form>
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 };
