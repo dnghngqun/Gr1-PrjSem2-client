bb22a462ca51e45c3e4690e5f7811c7a46f3c2e6
dothaofpt <143236377+dothaofpt@users.noreply.github.com>
2024-07-25 10:18:45 +0700

    up

--- my-app/src/Component/ProgressBar.js
+++ my-app/src/Component/ProgressBar.js
@@ -1,7 +1,13 @@
+
 import React from "react";
 import "./Css/ProgressBar.css";
 
 const ProgressBar = ({ sessions }) => {
+  // Đếm số buổi điểm danh theo trạng thái
+  const presentCount = sessions.filter(session => session.attendanceStatus === 'present').length;
+  const absentCount = sessions.filter(session => session.attendanceStatus === 'absent').length;
+  const totalCount = sessions.length;
+
   return (
     <div>
       <div className="progress-bar">
@@ -14,10 +20,12 @@ const ProgressBar = ({ sessions }) => {
       </div>
       <div className="note">
         <div className="note-child">
-          <span className="note-img note-green"></span> <span>Present</span>
+          <span className="note-img note-green"></span> 
+          <span>Present ({presentCount}/{totalCount})</span>
         </div>
         <div className="note-child">
-          <span className="note-img note-red"></span> <span>Absent</span>
+          <span className="note-img note-red"></span> 
+          <span>Absent ({absentCount}/{totalCount})</span>
         </div>
       </div>
     </div>
